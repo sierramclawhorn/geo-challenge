8d71fcc20a3650de700263c5bb2b7a68f30e911c
sierramclawhorn <sierramclawhorn@gmail.com>
2017-01-18 10:17:54 -0800

    Add map and markers for addresses

--- app/assets/javascripts/gmap.js
+++ app/assets/javascripts/gmap.js
@@ -1,11 +1,40 @@
 function initMap() {
+  var givenAddress = document.getElementById('address').innerText;
+  var storeAddress = document.getElementById('store').innerText;
+  var geocoder = new google.maps.Geocoder();
   var myLatLng = {lat: 37.7949, lng: -122.3700};
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 8,
     center: myLatLng
   });
-  var marker = new google.maps.Marker({
-    position: myLatLng,
-    map: map
+
+  // marker for given address
+  geocoder.geocode( {address:givenAddress}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      map.setCenter(results[0].geometry.location);//center the map over the result
+      //place a marker at the location
+      var marker = new google.maps.Marker(
+      {
+          map: map,
+          position: results[0].geometry.location
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+   }
+  });
+
+  // marker for closest store
+  geocoder.geocode( {address:storeAddress}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      map.setCenter(results[0].geometry.location);//center the map over the result
+      //place a marker at the location
+      var marker = new google.maps.Marker(
+      {
+          map: map,
+          position: results[0].geometry.location
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+   }
   });
 }
